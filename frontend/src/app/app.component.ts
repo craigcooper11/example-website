c7a47dc76855538cbfa68144caecd470ad36a21d
Craig Cooper <Craig.Cooper@ibm.com>
2023-08-18 10:32:49 -0500

    test

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
 
   onClick() {
     this.customers = [];
-    const test = this.http.get("http://localhost:3000");
+    const test = this.http.get("https://api-default.apps-crc.testing/");
     test.subscribe(users => {
       for (let [_, value] of Object.entries(users)) {
         const customer: Customer = {
@@ -41,7 +41,7 @@ export class AppComponent {
     }
     const postData = {name: this.firstName, description: this.description};
     console.log(postData);
-    this.http.post<any>('http://localhost:3000/customer', postData).subscribe(data => {})
+    this.http.post<any>('https://api-default.apps-crc.testing//customer', postData).subscribe(data => {})
     this.firstName = "";
     this.description = "";
   }
